d365959e0e8ad90d3ef428d6c6065acdc88d7ada
himonyon <goodbrther62@icloud.com>
2023-08-02 00:29:22 +0900

    init Project

---
+++ Game/HMKingdom/Source/HMKingdom/HMKingdomGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HMKingdomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HMKINGDOM_API AHMKingdomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
